ee82e820c1cd3dfe9f550193345878fca7721b46
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-28 22:55:19 +0530

    Add files via upload

--- Ticket_booking.java
+++ Ticket_booking.java
@@ -5,8 +5,10 @@ import java.util.function.Predicate;
 public class Ticket_booking {
 public static void main(String args[])
 {
-	Predicate<Integer> p=(no)->no>60;
-	System.out.println(p.test(55));
+//	Predicate<Integer> p=(no)->no>60;
+//	System.out.println(p.test(55));
+	Predicate<String> p=(no)->(no.length()>8);
+	System.out.println(p.test("abc213198"));
 }
 //public boolean age(int age)
 //{
