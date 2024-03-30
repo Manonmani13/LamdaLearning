3d4b34e7df1fed765304f51caa654d61f422a452
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-30 13:53:31 +0530

    Add files via upload

--- Ticket_booking.java
+++ Ticket_booking.java
@@ -1,5 +1,7 @@
 package com.lamda.learning;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.function.Predicate;
 
 public class Ticket_booking {
@@ -7,8 +9,13 @@ public static void main(String args[])
 {
 //	Predicate<Integer> p=(no)->no>60;
 //	System.out.println(p.test(55));
-	Predicate<String> p=(no)->(no.length()>8);
-	System.out.println(p.test("abc213198"));
+//	Predicate<String> p=(no)->(no.length()>8);
+//	System.out.println(p.test("abc213198"));
+	ArrayList al=new ArrayList();
+	al.add(10);
+	Predicate<Collection> p=aa->aa.size()>0;
+	System.out.println(p.test(al));
+		
 }
 //public boolean age(int age)
 //{
