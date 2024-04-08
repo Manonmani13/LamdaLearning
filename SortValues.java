093d36bc725ad755365aba27ec313c410a27e6ed
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-04-08 21:37:47 +0530

    Add files via upload

---
+++ SortValues.java
@@ -0,0 +1,19 @@
+package com.lamda.learning;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class SortValues {
+public static void main(String[] args) {
+	List<String> names=new ArrayList(); 
+	names.add("naveen");
+	names.add("manonmani");
+	names.add("chandar");
+	names.add("leena");
+	names.forEach(n->{System.out.println(n);
+	});
+	
+}
+}
