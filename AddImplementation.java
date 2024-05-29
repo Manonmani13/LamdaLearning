81c90fa4547ae4a58e8933e6212c6947b128c8c3
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-05-29 21:07:51 +0530

    Add files via upload

---
+++ AddImplementation.java
@@ -0,0 +1,8 @@
+package com.practices.java8;
+
+public class AddImplementation {
+public static void main(String[] args) {
+	AddInterface ai=(int a,int b)->System.out.print("Addition "+(a+b));
+	ai.add(20,30);
+}
+}
---
+++ AddInterface.java
@@ -0,0 +1,6 @@
+package com.practices.java8;
+
+public interface AddInterface {
+	public void add(int a,int b);
+
+}
