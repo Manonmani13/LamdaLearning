f47cb651cc3bfbb128a4d76fd2b03c9550e6e91a
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-05-29 22:37:10 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,10 @@
+package com.practices.java8;
+
+public class Square {
+public static void main(String[] args) {
+	SquareInterface s=n->{
+		return n*n;
+	};
+	System.out.println(s.square(3));
+}
+}
---
+++ SquareInterface.java
@@ -0,0 +1,6 @@
+package com.practices.java8;
+
+public interface SquareInterface {
+
+	public int square(int a);
+}
